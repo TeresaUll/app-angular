4a5b7851dded347626d327cc57a8ad0d28513473
TeresaUll <tbonetcosta@gmail.com>
2022-08-20 00:47:53 +0200

    error component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+// Components
+import { TasksComponent } from './components/tasks/tasks.component';
+import { PrivateTasksComponent } from './components/private-tasks/private-tasks.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tasks',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+  {
+    path: 'private',
+    component: PrivateTasksComponent,
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
